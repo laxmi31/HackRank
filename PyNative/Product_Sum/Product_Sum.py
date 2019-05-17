8c98293b55bf23d69a70498ba3791368165d70c5
laxmi31 <42066472+laxmi31@users.noreply.github.com>
2019-05-17 09:06:34 +0530

    Create Product_Sum.py

---
+++ PyNative/Product_Sum/Product_Sum.py
@@ -0,0 +1,8 @@
+a=int(input("enter the first number"))
+b=int(input("enter the second number"))
+c=a*b
+
+if(c>1000):
+  print("The sum since product is greater than 1000 is:",(a+b))
+else:
+  print("the product is:",c)
